709f6c93b82c7de2000ca2caf09a2d7f3b27dbf1
paulober <44974737+paulober@users.noreply.github.com>
2022-09-06 14:33:58 +0200

    Added linux permission solving bash script

---
+++ scripts/solvePermissions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This script will give your useraccount access to the serial port ttyACM0
+
+THEUSER=""
+
+if [ -v $SUDO_USER ]; then
+    THEUSER=$USER
+else
+    THEUSER=$SUDO_USER
+fi
+
+echo "You are ${THEUSER}..."
+# !INFO! change /dev/ttyACM0 to your serial port if it is different for you Pico
+GROUP=$(stat -c '%G' /dev/ttyACM0)
+echo "And get the '${GROUP}' group..."
+
+# add user to group
+sudo usermod -a -G $GROUP $THEUSER
+echo "Done! Please logout and login again to activate changes."
