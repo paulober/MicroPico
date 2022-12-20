40b4131157eb17b1206bf31f7e4d9909f5276d85
paulober <44974737+paulober@users.noreply.github.com>
2022-12-20 15:10:11 +0100

    Updated stubs to v1.19.1-780

--- stubs/stubs/umachine/umachine.pyi
+++ stubs/stubs/umachine/umachine.pyi
@@ -145,6 +145,30 @@ class Pin:
         """
         ...
 
+
+class RTC():
+    """The RTC is an independent clock that keeps track of the date and time."""
+
+    def __init__(self, *argv, **kwargs) -> None:
+        ...
+
+    def datetime(self, *args, **kwargs) -> Any:
+        """
+        Get or set the date and time of the RTC.
+
+        With no arguments, this method returns an 8-tuple with the 
+        current date and time. With 1 argument (being an 8-tuple) 
+        it sets the date and time.
+
+        The 8-tuple has the following format:
+
+        (year, month, day, weekday, hours, minutes, seconds, subseconds)
+
+        The meaning of the `subseconds` field is hardware dependent.
+        """
+        ...
+
+
 class SPI:
     """
     SPI is a synchronous serial protocol that is driven by a master. At the
@@ -853,6 +877,99 @@ class I2C:
         from and written to.  In this case there are two addresses associated with an
         I2C transaction: the slave address and the memory address.  The following
         methods are convenience functions to communicate with such devices.
+        """
+        ...
+
+
+class I2S():
+    """
+    I2S is a synchronous serial protocol used to connect digital audio devices. 
+    At the physical level, a bus consists of 3 lines: SCK, WS, SD. The I2S class 
+    supports controller operation. Peripheral operation is not supported.
+
+    The I2S class is currently available as a Technical Preview. During the preview 
+    period, feedback from users is encouraged. Based on this feedback, the I2S class 
+    API and implementation may be changed.
+    """
+
+    def __init__(self, *argv, **kwargs) -> None:
+        """
+        Construct an I2S object of the given id:
+
+        id identifies a particular I2S bus; it is board and port specific
+
+        Keyword-only parameters that are supported on all ports:
+
+        - `sck` is a pin object for the serial clock line
+
+        - `ws` is a pin object for the word select line
+
+        - `sd` is a pin object for the serial data line
+
+        - `mck` is a pin object for the master clock line; master clock frequency is sampling rate * 256
+
+        - `mode` specifies receive or transmit
+
+        - `bits` specifies sample size (bits), 16 or 32
+
+        - `format` specifies channel format, STEREO or MONO
+
+        - `rate` specifies audio sampling rate (Hz); this is the frequency of the ws signal
+
+        - `ibuf` specifies internal buffer length (bytes)
+        """
+        ...
+
+    def readinto(self, buf) -> int:
+        """
+        Read audio samples into the buffer specified by `buf`. `buf` must 
+        support the buffer protocol, such as bytearray or array. "buf" 
+        byte ordering is little-endian. For Stereo format, left channel 
+        sample precedes right channel sample. For Mono format, the left 
+        channel sample data is used. Returns number of bytes read
+        """
+        ...
+
+    def write(self, buf) -> int:
+        """
+        Write audio samples contained in `buf`. `buf` must support the buffer protocol, 
+        such as bytearray or array. “buf” byte ordering is little-endian. For Stereo 
+        format, left channel sample precedes right channel sample. For Mono format, 
+        the sample data is written to both the right and left channels. 
+        Returns number of bytes written
+        """
+        ...
+
+    MONO = 0 # type: int
+    RX = 0 # type: int
+    STEREO = 1 # type: int
+    TX = 1 # type: int
+
+    def deinit(self, *args, **kwargs) -> Any:
+        """Deinitialize the I2S bus"""
+        ...
+
+    def init(self, *args, **kwargs) -> Any:
+        ...
+
+    def irq(self, handler) -> Any:
+        """
+        Set a callback. `handler` is called when `buf` is 
+        emptied (`write` method) or becomes full 
+        (`readinto` method). Setting a callback changes 
+        the `write` and `readinto` methods to non-blocking 
+        operation. `handler` is called in the context 
+        of the MicroPython scheduler.
+        """
+        ...
+
+    def shift(self, buf, bits, shift: int) -> Any:
+        """
+        bitwise shift of all samples contained in `buf`. `bits` 
+        specifies sample size in bits. `shift` specifies the number 
+        of bits to shift each sample. Positive for left shift, 
+        negative for right shift. Typically used for volume control. 
+        Each bit shift changes sample volume by 6dB.
         """
         ...
 
