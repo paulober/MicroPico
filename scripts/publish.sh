e717fb75db934ab605cf765adc56eaf38247f848
paulober <44974737+paulober@users.noreply.github.com>
2024-09-09 09:02:02 +0100

    Fix publish/package workflow for macOS
    Signed-off-by: paulober <44974737+paulober@users.noreply.github.com>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -15,10 +15,18 @@ chmod +x "$SCRIPT_DIR/package.sh"
 
 # Find all .vsix files except the one without a platform prefix and publish them one by one
 find . -type f -name "micropico-$RELEASE_TAG_NAME-*.vsix" ! -name "micropico-$RELEASE_TAG_NAME.vsix" | while read file -r package_path; do
+  # Default target flags (for non-darwin platforms)
+  target_flags=""
+
+  # If the filename contains "darwin", add the appropriate target flags
+  if [[ "$package_path" == *"darwin"* ]]; then
+    target_flags="--target darwin-x64 darwin-arm64 "
+  fi
+
   # Publish the VSCode extension to the VSCode Marketplace
-  npx @vscode/vsce publish --packagePath "$package_path"
+  npx @vscode/vsce publish $target_flags--packagePath "$package_path"
   # Publish the VSCode extension to the Open VSX Registry
-  npx ovsx publish "$package_path" -p "$OVSX_PAT"
+  npx ovsx publish "$package_path" $target_flags-p "$OVSX_PAT"
   # delete this vsix file
   rm -rf "$package_path"
 done
